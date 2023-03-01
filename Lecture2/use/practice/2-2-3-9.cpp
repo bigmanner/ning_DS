a4b147129becc076cc1d1c69ff1f735d2b1890a5
shelley <1090256107@qq.com>
2023-03-01 14:11:52 +0800

    ning's commit for update wangdao 2-2-3-9,2023-03-01

--- Lecture2/use/practice/2-2-3-9.cpp
+++ Lecture2/use/practice/2-2-3-9.cpp
@@ -1,55 +1,79 @@
 //包含自定义头文件
-#include"SeqListClass.h"
+#include"CommonFunction.h"
 
 //包含系统头文件
 #include<iostream>
-#include<algorithm>
 using namespace std;
 
-int BinarySearch(int a[],int n,ElementTypeS x,int &insertLoc)
+//全局变量
+ComFunction comFunc;
+
+bool exchangeOrInsert(int a[],int n,int searchElement)
 {
-    //二分查找函数，insertLoc仅在返回值为-1时生效
-    //返回的值是位序值，不是下标值
+    //如果能查找到元素，则和后继元素位置交换，如果找不到，插入到合适的位置
+    //返回true表示查找到了元素，返回false表示没有查找到元素，并打算在合适的位置进行添加
+    //利用二分查找T(n)=O(log2n)
+    //接着继续吧
     int left=0;
-    int right=n-1;//传入长度
+    int right=n-1;
+    int mid=0;
+    int flag=0;             //用以指示最后一步是left=mid+1还是right=mid-1
+    int insertIndex=-1;     //需要插入的位置
     while(left<=right)
     {
-        //只要left小于right，循环就能继续
-        int mid=(left+right)/2;
-        if(x==a[mid])
+        mid=(left+right)/2;
+        if(searchElement==a[mid])
         {
-            return mid+1;
+            //说明找到了,交换前后两个元素的位置
+            if(n!=1&&mid!=n-1)
+            {
+                //如果交换前后两个元素，必须保证mid指示的元素不是最后一个或者只有一个元素
+                int temp=0;
+                temp=a[mid];
+                a[mid]=a[mid+1];
+                a[mid+1]=temp;
+            }
+            return true;
         }
-        if(x<a[mid])
+        else if(searchElement<a[mid])
         {
             right=mid-1;
-            if(left>=right)
-            {
-                insertLoc=left+1;//+1代表位序
-            }
-            continue;
+            flag=0;
         }
-        if(x>a[mid])
+        else if(searchElement>a[mid])
         {
             left=mid+1;
-            if(left>=right)
-            {
-                insertLoc=right+1;//此处加一代表位序
-            }
-            continue;
+            flag=1;
         }
     }
-    //如果跳出该循环，说明x不在这个数组里面，直接return -1即可
-    return -1;
+    //即使下标是-1或者n需要插入，都是需要满足的
+    //这里right应该等于-1？？？
+    if(flag==0)
+    {
+        insertIndex=right+1;
+        //cout<<"flag为0，insertIndex值为"<<insertIndex<<endl;
+    }
+    else if(flag==1)
+    {
+        insertIndex=left;
+        //cout<<"flag为1，insertIndex值为"<<insertIndex<<endl;
+    }
+    //依次向后移动元素
+    for(int i=n-1;i>=insertIndex;i--)
+    {
+        a[i+1]=a[i];
+    }
+    a[insertIndex]=searchElement;
+    return false;
 }
 int main()
 {
-    //二分查找函数测试
-    int a[10]={1,2,4,5,6,10,12,14,15,16};
-    //sort(a,a+10);
-    int insertLoc=-1;
-    int findLoc=BinarySearch(a,10,3,insertLoc);
-    cout<<"13的findLoc位置是"<<findLoc<<endl;
-    cout<<"13的insertLoc是"<<insertLoc<<endl;
+    int A[10]={1,3,5,7,0,0,0,0,0,0};
+    comFunc.ArrayPrint(A,4);
+    int n=4;
+    //int result=comFunc.ArrayBinarySearch(A,4,6);
+    exchangeOrInsert(A,4,5);
+    comFunc.ArrayPrint(A,5);
+    //cout<<"二分查找结果为"<<result<<endl;
     return 0;
 }
